f3031bed5ee3eada4ed25133a99066dfb40654a2
markgolubev <markflyingbear@gmail.com>
2023-02-13 12:37:30 +0000

    Initial Commit

---
+++ realmTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  realmTest
+//
+//  Created by Марк Голубев on 13.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
