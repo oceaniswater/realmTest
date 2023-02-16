3ae111da9c9978ed889514b89dc9d0ea13a509d3
markgolubev <markflyingbear@gmail.com>
2023-02-16 14:32:29 +0000

    add editing functional to list of category using SwipeCellKit

--- realmTest/Controller/CategoryViewController.swift
+++ realmTest/Controller/CategoryViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RealmSwift
+import SwipeCellKit
 
 class CategoryViewController: UITableViewController {
     
@@ -17,6 +18,7 @@ class CategoryViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.rowHeight = 80.0
         loadCategories()
         
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
@@ -26,7 +28,7 @@ class CategoryViewController: UITableViewController {
         
     }
     
-    
+    // MARK: - Action controllers
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         // created controller
         let ac = UIAlertController(title: "Add Category to your to do list", message: nil, preferredStyle: .alert)
@@ -63,8 +65,50 @@ class CategoryViewController: UITableViewController {
         //        self.tableView.insertRows(at: [indexPath], with: .automatic)
         return
     }
-    //
-    //        // MARK: - CRUD for Realm
+    
+    func updateAC(indexPath: IndexPath) {
+        // created controller
+        let ac = UIAlertController(title: "Update category name", message: nil, preferredStyle: .alert)
+        // added TextField
+        ac.addTextField()
+        ac.textFields?[0].text = categories?[indexPath.row].name
+        
+        
+        // created update button
+        let updateAction = UIAlertAction(title: "Update", style: .default) {
+            // trying to avoid strong reference
+            [weak self, weak ac] action in
+            // checked textField is not nil
+            guard let newName = ac?.textFields?[0].text, newName != "" else { return }
+            // use button Submit using answer and method out of closure
+            if let category = self?.categories?[indexPath.row] {
+                do {
+                    try self?.realm.write({
+                        category.name = newName
+                    })
+                } catch {
+                    print("error updating done status \(error.localizedDescription)")
+                }
+                self?.tableView.reloadData()
+            }
+        }
+        
+        // created dismis button
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive) {
+            // trying to avoid strong reference
+            [weak self] action in
+            // checked textField is not nil
+            self?.tableView.reloadData()
+            return
+        }
+        
+        ac.addAction(updateAction)
+        ac.addAction(cancelAction)
+        present(ac, animated: true)
+    }
+    
+    
+    // MARK: - CRUD for Realm
     
     func save(category: Category) {
         
@@ -105,11 +149,12 @@ class CategoryViewController: UITableViewController {
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! SwipeTableViewCell
         var content = cell.defaultContentConfiguration()
         content.image = UIImage(systemName: "puzzlepiece.fill")
         content.text = categories?[indexPath.row].name ?? "No category added"
         cell.contentConfiguration = content
+        cell.delegate = self
         
         return cell
     }
@@ -119,21 +164,21 @@ class CategoryViewController: UITableViewController {
     }
     
     // add delete cell functional
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            if let category = categories?[indexPath.row] {
-                do {
-                    try realm.write({
-                        realm.delete(category.items)
-                        realm.delete(category)
-                    })
-                } catch {
-                    print("error updating done status \(error.localizedDescription)")
-                }
-            }
-            tableView.deleteRows(at: [indexPath], with: .bottom)
-        }
-    }
+    //    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    //        if editingStyle == .delete {
+    //            if let category = categories?[indexPath.row] {
+    //                do {
+    //                    try realm.write({
+    //                        realm.delete(category.items)
+    //                        realm.delete(category)
+    //                    })
+    //                } catch {
+    //                    print("error updating done status \(error.localizedDescription)")
+    //                }
+    //            }
+    //            tableView.deleteRows(at: [indexPath], with: .bottom)
+    //        }
+    //    }
     
     // MARK: - Navigation
     
@@ -146,8 +191,61 @@ class CategoryViewController: UITableViewController {
             }
         }
         
-
+        
     }
     
     
 }
+
+// MARK: - SwipeTableViewCellDelegate
+
+extension CategoryViewController: SwipeTableViewCellDelegate  {
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+        
+        let editAction = SwipeAction(style: .default, title: "Edit", handler: { action, indexPath in
+            self.updateAC(indexPath: indexPath)
+        })
+        
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
+            // handle action by updating model with deletion
+            if let category = self.categories?[indexPath.row] {
+                do {
+                    try self.realm.write({
+                        self.realm.delete(category.items)
+                        self.realm.delete(category)
+                    })
+                } catch {
+                    print("error updating done status \(error.localizedDescription)")
+                }
+            }
+//            tableView.deleteRows(at: [indexPath], with: .bottom)
+        }
+        
+        // customize the action appearance
+        editAction.image = UIImage(systemName: "pencil")?.resized(to: CGSize(width: 35, height: 35))?.withTintColor(.white)
+        editAction.backgroundColor = .orange
+        deleteAction.image = UIImage(named: "delete-icon")
+        
+        return [deleteAction, editAction]
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+        options.transitionStyle = .border
+        return options
+    }
+    
+}
+// MARK: - UIImage resize method
+
+extension UIImage {
+    func resized(to size: CGSize) -> UIImage? {
+        let renderer = UIGraphicsImageRenderer(size: size)
+        return renderer.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: size))
+        }
+    }
+}
+
